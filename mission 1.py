844bc4b7c464cc9075efb5c6df496ee5ea8dbc1c
AL <aladvena@gmail.com>
2023-05-29 18:38:27 +0300

    Initial commit

---
+++ mission 1.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_smartest(heroes_list):
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    data = requests.get(url)
+    smartest_hero_dict = {}
+    for hero in data.json():
+        for smartest_hero in heroes_list:
+            if hero['name'] == smartest_hero:
+                intelligence = hero['powerstats']['intelligence']
+                smartest_hero_dict[smartest_hero] = intelligence
+    return max(smartest_hero_dict, key=smartest_hero_dict.get)
+
+print(f"Самый умный супергерой: {get_smartest(['Hulk', 'Captain America', 'Thanos'])}")
---
+++ mission 2.py
@@ -0,0 +1,30 @@
+import requests
+
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {"content-type": "application/json",
+                "Authorization": f'OAuth {self.token}'}
+
+    def _get_upload_link(self, disk_space_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_space_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_space_path=disk_file_path).get('href', '')
+        response = requests.put(href, data=open(filename, 'rb'))
+        return 'Выполнено' if response.status_code == 201 else 'Ошибка'
+
+
+if __name__ == '__main__':
+    path_to_file = 'test_file.txt'
+    filename = 'test_file.txt'
+    token = ' '
+    up = YaUploader(token)
+    result = up.upload(path_to_file, filename)
+    print(result)
